60f6a4df243b3db29da477cab2cf576af9d04fcb
Abubakir Bohodirov <abubakrbohodirov185@gmail.com>
2024-09-19 21:50:08 +0500

    done

---
+++ main.py
@@ -0,0 +1,27 @@
+# 1. 7ta raqamdan iborat list yarating va undagi eng katta raqamni chop eting
+
+num = [1, 2, 3, 4, 5, 6, 7]
+
+katta_son = max(num)
+print(katta_son)
+
+# 2. 7ta raqamdan iborat list yarating va undagi eng kichik raqamni chop eting
+
+kichik_son = min(num)
+print(kichik_son)
+
+# 3. Yuqoridagi listdagi elementlarni teskari tartibda chiqaring
+
+num.reverse()
+print(num)
+
+# 4. Ushbu ro'yxatdagi raqamlarni kichikdan kattasiga qarab saralab chiqaring
+
+num2 = [3, 7, 1, 5, 2, 6, 4 ]
+
+num2.sort()
+
+# 5. nums ro'yxati nechta elementdan tashkil topganligini aniqlab chiqaring.
+
+uzunligi = len(num)
+print(uzunligi)
